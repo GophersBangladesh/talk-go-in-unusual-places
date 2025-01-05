7dc03b53e85c516ce1a0c54e6b0f10b1e0d34a24
Omran Jamal <o.jamal97@gmail.com>
2025-01-05 15:05:08 +0600

    even simpler server code

--- 01-on-android/the-package/server.go
+++ 01-on-android/the-package/server.go
@@ -11,12 +11,5 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 func Serve() {
 	http.HandleFunc("/", helloHandler)
-
-	fmt.Println("Starting server at port 8080...")
-
-	err := http.ListenAndServe(":8080", nil)
-
-	if err != nil {
-		fmt.Println("Error starting server:", err)
-	}
+	http.ListenAndServe(":8080", nil)
 }
